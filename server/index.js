1b2b0504e65530751158248e323abb81ad062e93
ingenie <ingenie@hyundai-autoever.com>
2020-11-17 09:41:56 +0900

    Server

---
+++ server/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const cors = require('cors');
+const monk = require('monk');
+
+const app = express();
+const db = monk(process.env.MONGO_ADDR || 'localhost/chat');
+const port = process.env.PORT || 4000;
+const chat = db.get('chat');
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res)=> {
+    res.json({
+        message : 'open chat',
+    });
+});
+
+app.get('/chat', (req, res, next) =>{
+    chat.find().then((msg) =>{
+        res.json(msg);
+    });
+});
+
+app.post('/chat', (req, res, next) =>{
+    const msg = {
+        name : req.body.name.toString(),
+        comment : req.body.comment.toString(),
+        createdDate : new Date(),
+    };
+    chat.insert(msg).then((createdMsg) => {
+        res.json(createdMsg);
+    })
+});
+
+app.listen(port, () =>{
+    console.log(`Listening on ${port}`);
+})
